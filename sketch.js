9ac434bbd2c868a4615bebd9c2c83b3c508a4642
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-03-25 17:40:03 -0700

    final update

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+let r = 0;
+let g = 0;
+let b = 0;
+
+let redButton;
+let greenButton;
+let blueButton;
+
 let shape = "line";
 let noiseOffset = 0.0;
 let strokeWidth = 5;
@@ -5,49 +13,83 @@ let strokeWidth = 5;
 function setup() {
   createCanvas(1000, 1000);
   background(255);
+
+  //red button
+  redButton = createButton('Change To Red');
+  redButton.position(17,364);
+  redButton.size(100,50);
+  redButton.style('background-color', color(255,0,0));
+  redButton.mousePressed(redColor);
+//green button
+  greenButton = createButton('Change To Green');
+  greenButton.position(17,464);
+  greenButton.size(100,50);
+  greenButton.style('background-color', color(0,255,0));
+  greenButton.mousePressed(greenColor);
+  //blue button
+  blueButton = createButton('Change To Blue');
+  blueButton.position(17,564);
+  blueButton.size(100,50);
+  blueButton.style('background-color', color(0,0,255));
+  blueButton.mousePressed(blueColor);
+
   drawGrid();
   noFill();
 }
 
 function draw() {
   strokeWeight(strokeWidth);
-
   noiseOffset += 0.10;
   strokeWidth = noise(noiseOffset) * 50;
+
   if (mouseIsPressed) {
-    stroke(map(mouseX, 0, 1000, 0, 255, true));
+    noFill();
+    stroke(color(r,g,b));
     if (shape == "line") {
       line(mouseX, mouseY, pmouseX, pmouseY);
     } else if (shape == "ellipse") {
-      ellipse(mouseX, mouseY, pmouseX, pmouseY);
+      strokeWeight(3);
+      ellipse(mouseX, mouseY, 100, 100);
     } else if (shape == "rectangle") {
-      rect(mouseX, mouseY, pmouseX, pmouseY);
+      strokeWeight(3);
+      rect(mouseX, mouseY, 100, 100);
     }
-  } // else {
-  //   push();
-  //   textSize(25);
-  //   fill(random(255), random(255), random(255));
-  //   text('What Will You Create', mouseX, mouseY);
-  //   pop();
-  // }
+  }
 }
 
 function keyTyped() {
 
   if (key === 's') {
     saveCanvas('fileName', 'png');
+  } else if (key === 'l'){
+    shape = "line";
   } else if (key === 'c') {
     shape = "ellipse";
   } else if (key === 'r') {
     shape = "rectangle";
   } else if (key === ' ') {
     clear();
+    drawGrid();
   }
 }
 
-// function mousePressed() {
-//
-// }
+function redColor(){
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function greenColor(){
+  r = 0;
+  g = 255;
+  b = 0;
+}
+
+function blueColor(){
+  r = 0;
+  g = 0;
+  b = 255;
+}
 
 function drawGrid() {
   numCells = 20;
@@ -56,7 +98,7 @@ function drawGrid() {
 
   for (let i = 0; i <= width; i += width / numCells) {
     for (let j = 0; j <= height; j += height / numCells) {
-      if (fillColor === 255){
+      if (fillColor === 255) {
         fillColor = 200;
       } else {
         fillColor = 255;
